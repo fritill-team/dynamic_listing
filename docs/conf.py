d8b854f51a1266a5b585a2daa3f26cc55d582586
Sohype-Khaled <sohype.mop@gmail.com>
2023-08-31 12:03:59 +0300

    update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Sohype Khaled'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['python_docs_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
